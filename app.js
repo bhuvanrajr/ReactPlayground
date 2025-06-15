93b5223a044358c6b81e0a5e5cf2a97d2c84fb72
Bhuvan Raj R <HSE8@eurofins.com>
2025-06-15 23:19:32 +0530

    Updated Parchel config end of 2nd Episode.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
- var header = React.createElement("h1",{id:"header"},"I am from React!");
+ import React from "react"
+ import ReactDOM from "react-dom/client"
+ 
+ var header = React.createElement("h1",{id:"header"},"I am from Parcel!");
 var root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(header);
